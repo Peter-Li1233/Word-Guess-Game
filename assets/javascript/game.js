bea20c0520817cd23847ba93a2fa74106719c854
Peter-Li1233 <minxiangli08@gmail.com>
2018-06-06 16:04:23 -0400

    not include object yet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,6 +10,7 @@ window.onload = function() {
     
 
     // put the word chosen in wordChosenarray; word guessed in wordGuessarray
+    // Define an arrany "letterTried" to store letters tried
     var wordChosenarray = wordChosen.split("");
         console.log(wordChosenarray);
     var arrayLength = wordChosenarray.length;
@@ -17,6 +18,14 @@ window.onload = function() {
     var wordGuessarray = [];
         wordGuessarray.length=arrayLength;
         console.log(wordGuessarray.length);
+    var letterTried = [];
+
+    // var parent3 = document.getElementById("div3");
+    //     console.log(parent3);
+    // var child3  = document.getElementById("p3");
+    //     console.log(child3);
+    var numOfLeft = 10;
+    var numOfWins = 0;
 
         for (i=0; i<arrayLength; i++) {
             wordGuessarray[i] = " _ ";
@@ -31,55 +40,122 @@ window.onload = function() {
             element.appendChild(nodeText);
         }
 
+        document.getElementById("wins").innerHTML = numOfWins;
+        document.getElementById("p2").innerHTML = numOfLeft;
+
     
     window.onkeyup = function(event) {
+        numOfLeft--;
+        document.getElementById("p2").innerHTML = numOfLeft;
+
+        var parent1 = document.getElementById("div1");
+        console.log(parent3);
+        var child1  = document.getElementById("p1");
+        console.log(child3);
+        var parent3 = document.getElementById("div3");
+        console.log(parent3);
+        var child3  = document.getElementById("p3");
+        console.log(child3);
+
+        
+
         var keyTyped = event.key;
         console.log(keyTyped);
 
         var keyIndexes;
 
-        if (wordChosenarray.includes(keyTyped) ) {
-            keyIndexes = getAllIndexes(wordChosenarray,keyTyped);
-            console.log (keyIndexes);
+        if (!letterTried.includes(keyTyped)) {
+            letterTried.push(keyTyped);
+            console.log(letterTried);
+            
+            displayArray(letterTried,parent3,child3);
 
-            pushWordGuess(keyIndexes);
-            console.log(wordGuessarray);
+            if (wordChosenarray.includes(keyTyped) ) {
+                keyIndexes = getAllIndexes(wordChosenarray,keyTyped);
+                console.log (keyIndexes);
 
-            replaceWordGuess(wordGuessarray);           
+                pushWordGuess(keyIndexes);
+                console.log(wordGuessarray);
+
+                
+
+                //replaceWordGuess(wordGuessarray);
+                displayArray(wordGuessarray,parent1,child1);
+
+                console.log (arrayEqual(wordChosenarray,wordGuessarray));
+                if (arrayEqual(wordChosenarray,wordGuessarray)) {
+                    numOfWins++;
+                    document.getElementById("wins").innerHTML = numOfWins;
+                    alert("you win");
+                }
+                
+            } else {
+                //alert("you lost 1 live");
+                if (numOfLeft<=0) {
+                    alert("lost")
+                }
+            }
         } else {
-            alert("you lost 1 live!");
-        }
-
-        function getAllIndexes(arr,val) {
-            var indexes = [], i= -1;
-            while ((i =arr.indexOf(val,i+1)) != -1) {
-                indexes.push(i);
-            }
-            return indexes;
-        } 
-        function pushWordGuess(indexes) {
-            for (i=0; i<indexes.length;i++) {
-                var index = parseInt(indexes[i]);
-                wordGuessarray[index]=keyTyped;
+            //alert("you lost 1 live!");
+            if (numOfLeft<=0) {
+                alert("lost");
             }
         }
-        function replaceWordGuess(arr) {
-            var parent = document.getElementById("div1");
-            console.log(parent);
-            var oldChild = document.getElementById("p1");
-            console.log(p1);
-            var newChild = document.createElement("p")
-                newChild.setAttribute("id","p1");
 
-            for (i=0; i<arr.length; i++) {
-                var content = arr[i];
-                console.log(content);
-                var textNode = document.createTextNode(content);   
-                newChild.appendChild(textNode);    
-                }      
-            parent.replaceChild(newChild,oldChild);
-        }
-        
+            function getAllIndexes(arr,val) {
+                var indexes = [], i= -1;
+                while ((i =arr.indexOf(val,i+1)) != -1) {
+                    indexes.push(i);
+                }
+                return indexes;
+            } 
+            function pushWordGuess(indexes) {
+                for (i=0; i<indexes.length;i++) {
+                    var index = parseInt(indexes[i]);
+                    wordGuessarray[index]=keyTyped;
+                }
+            }
+            // function replaceWordGuess(arr) {
+            //     var parent = document.getElementById("div1");
+            //     console.log(parent);
+            //     var oldChild = document.getElementById("p1");
+            //     console.log(p1);
+            //     var newChild = document.createElement("p");
+            //         newChild.setAttribute("id","p1");
+
+            //     for (i=0; i<arr.length; i++) {
+            //         var content =" " + arr[i] + " ";
+            //         console.log(content);
+            //         var textNode = document.createTextNode(content);   
+            //         newChild.appendChild(textNode);    
+            //         }      
+            //     parent.replaceChild(newChild,oldChild);
+            // }
+            function displayArray(arr,parent,child) {
+                var oldChild = child;
+                    console.log(oldChild);
+                var oldAttribute = oldChild.getAttribute("id");
+                    console.log(oldAttribute);
+                var newChild = document.createElement("p");
+                    newChild.setAttribute("id", oldAttribute);
+
+                    for (i=0; i<arr.length; i++) {
+                        var content =" " + arr[i] + " ";
+                        console.log(content);
+                        var textNode = document.createTextNode(content);   
+                        newChild.appendChild(textNode);    
+                        } 
+                        console.log(newChild);     
+                    parent.replaceChild(newChild,oldChild);
+            }
+            function arrayEqual(arr1,arr2) {
+                for (var i=0; i<arr1.length; i++){
+                    if (arr1[i] !== arr2[i]) {
+                        return false;
+                    }
+                } 
+                return true;
+            }
     }
     
 }
